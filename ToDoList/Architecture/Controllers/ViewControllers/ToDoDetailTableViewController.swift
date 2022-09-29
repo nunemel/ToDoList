5c64282e1159a6b3d9ac9f9cefa9613ec58ecff9
nunemel <nune.melikyan@yahoo.com>
2022-09-29 17:20:24 +0400

    Add share with mail functionality.

--- ToDoList/Architecture/Controllers/ViewControllers/ToDoDetailTableViewController.swift
+++ ToDoList/Architecture/Controllers/ViewControllers/ToDoDetailTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 final class ToDoDetailTableViewController: UITableViewController {
 
@@ -15,6 +16,7 @@ final class ToDoDetailTableViewController: UITableViewController {
   @IBOutlet var dueDateDatePicker: UIDatePicker!
   @IBOutlet var notesTextView: UITextView!
   @IBOutlet var saveButton: UIBarButtonItem!
+  @IBOutlet private var shareButton: UIButton!
 
   private var isDatePickerHidden = true
   private let dateLabelIndexPath = IndexPath(row: 0, section: 1)
@@ -135,4 +137,111 @@ final class ToDoDetailTableViewController: UITableViewController {
       )
     }
   }
+}
+
+
+
+extension ToDoDetailTableViewController: MFMailComposeViewControllerDelegate {
+        
+  @IBAction func sendEmail(_ sender: UIButton) {
+      guard let title = toDo?.title,
+        let isComplete = toDo?.isComplete,
+        let dueDate = toDo?.dueDate
+      else { return }
+
+      let toDoDescription =
+        "Title: \(title), isComplete: \(isComplete), duDate: \(dueDate)"
+    // Modify following variables with your text / recipient
+    let recipientEmail = "test@email.com"
+    let subject = "Share To Do task \(title)"
+      
+    let body =
+      toDoDescription
+
+    // Show default mail composer
+    if MFMailComposeViewController.canSendMail() {
+      let mail = MFMailComposeViewController()
+      mail.mailComposeDelegate = self
+      mail.setToRecipients([recipientEmail])
+      mail.setSubject(subject)
+      mail.setMessageBody(body, isHTML: false)
+
+      present(mail, animated: true)
+
+      // Show third party email composer if default Mail app is not present
+    }
+    else if let emailUrl = createEmailUrl(
+      to: recipientEmail,
+      subject: subject,
+      body: body
+    ) {
+      UIApplication.shared.open(emailUrl)
+    }
+  }
+
+  private func createEmailUrl(
+    to: String,
+    subject: String,
+    body: String
+  ) -> URL? {
+    let subjectEncoded = subject.addingPercentEncoding(
+      withAllowedCharacters: .urlQueryAllowed
+    )!
+    let bodyEncoded = body.addingPercentEncoding(
+      withAllowedCharacters: .urlQueryAllowed
+    )!
+
+    let gmailUrl = URL(
+      string:
+        "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)"
+    )
+    let outlookUrl = URL(
+      string:
+        "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)"
+    )
+    let yahooMail = URL(
+      string:
+        "ymail://mail/compose?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)"
+    )
+    let sparkUrl = URL(
+      string:
+        "readdle-spark://compose?recipient=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)"
+    )
+    let defaultUrl = URL(
+      string:
+        "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)"
+    )
+
+    if let gmailUrl = gmailUrl,
+      UIApplication.shared.canOpenURL(gmailUrl)
+    {
+      return gmailUrl
+    }
+    else if let outlookUrl = outlookUrl,
+      UIApplication.shared.canOpenURL(outlookUrl)
+    {
+      return outlookUrl
+    }
+    else if let yahooMail = yahooMail,
+      UIApplication.shared.canOpenURL(yahooMail)
+    {
+      return yahooMail
+    }
+    else if let sparkUrl = sparkUrl,
+      UIApplication.shared.canOpenURL(sparkUrl)
+    {
+      return sparkUrl
+    }
+
+    return defaultUrl
+  }
+
+  func mailComposeController(
+    _ controller: MFMailComposeViewController,
+    didFinishWith result: MFMailComposeResult,
+    error: Error?
+  ) {
+    controller.dismiss(animated: true)
+  }
+
 }
